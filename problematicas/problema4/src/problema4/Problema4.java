772680ab9398e46d70991ede142f46c7bf36df5c
caritoem <caritomazonespinosa@gmail.com>
2025-11-20 12:53:41 -0500

    cuarto

--- problematicas/problema4/src/problema4/Problema4.java
+++ problematicas/problema4/src/problema4/Problema4.java
@@ -18,7 +18,29 @@ public class Problema4 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        
+        int numero = 1;
+        int impares = 3;
+        int contador = 1;
+        int contadorsigno = 1;
+        int limite = 7;
+        String signo = "";
+        String mensajeF = "";
+
+        while (contador <= limite) {
+            if (contadorsigno %2 == 0){
+                signo = "-";
+            }else{
+                signo = "+";
+            }
+           
+            mensajeF = String.format("%s%s%d/%d", mensajeF, signo, numero, impares);
+
+            contador = contador + 1;
+            contadorsigno = contadorsigno + 1;
+            impares = impares + 2;
+        }
+
+        System.out.printf("%d %s", numero, mensajeF);
         
         
     }
