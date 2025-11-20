87b8a9466d3f3bcf5125244a30fc9d528dfc2ade
caritoem <caritomazonespinosa@gmail.com>
2025-11-20 11:48:26 -0500

    primero

--- problematicas/problemaa1/src/problemaa1/Problemaa1.java
+++ problematicas/problemaa1/src/problemaa1/Problemaa1.java
@@ -19,7 +19,49 @@ public class Problemaa1 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        
+        String mensajeF = "";
+        String nombre;
+        int edad;
+        String posicion;
+        double estatura;
+        int contador = 1;
+        int edadT;
+        int sumaE = 0;
+        double estaturaT;
+        double sumaES = 0;
+        int limite = 5;
+
+        while (contador <= limite) {
+
+            System.out.println("Ingrese nombre del jugador");
+            nombre = entrada.nextLine();
+
+            System.out.println("Ingrese su posición");
+            posicion = entrada.nextLine();
+
+            System.out.println("Ingrese su edad");
+            edad = entrada.nextInt();
+
+            System.out.println("Ingrese su estatura");
+            estatura = entrada.nextDouble();
+
+            sumaE = sumaE + edad;
+            sumaES = sumaES + estatura;
+
+            mensajeF = String.format("%s %s - %s -, edad %d, estatura %.2f\n",
+                     mensajeF, nombre, posicion, edad, estatura);
+
+            entrada.nextLine();
+            contador = contador + 1;
+
+        }
+
+        edadT = sumaE / 5;
+        estaturaT = sumaES / 5;
+
+        System.out.printf("Listado de jugadores:\n%s\n"
+                + "Promedio total de edades: %d\n"
+                + "Promedio total de estatura: %.2f", mensajeF, edadT, estaturaT);
     }
     
 }
